8a4c195a07811a5e2b7135ea20ceb2a5089401fd
Dwi Fahni Denni <dfdenni0912@gmail.com>
2022-12-11 12:59:14 +0700

    Refactoring builder script container

--- dockerhub-build.sh
+++ dockerhub-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # -----------------------------------------------------------------------------
-#  Docker Push Container (DockerHub)
+#  Docker Build Container (DockerHub)
 # -----------------------------------------------------------------------------
 #  Author     : Dwi Fahni Denni
 #  License    : Apache v2
@@ -11,36 +11,67 @@ set -e
 # export CI_PROJECT_NAME="nifi"
 
 # export IMAGE="$CI_PROJECT_PATH/$CI_PROJECT_NAME"
-export IMAGE=$4
+export IMAGE=$2
 
 docker_build() {
-  export TAGS_ID=$1
-  export FILE=$2
-  export CUSTOM_TAGS=$3
+  export FILE=$1
+  export BASE_IMAGE=$3
+  export TAGS_ID=$4
+  export CUSTOM_TAGS=$5
 
   if [ "$CUSTOM_TAGS" = "" ]; then
+    echo "Build Image => $IMAGE:${BASE_IMAGE}"
+    echo ">> docker build -t $IMAGE:${BASE_IMAGE} -f $FILE ."
+    docker build -t $IMAGE:${BASE_IMAGE} -f $FILE .
+    echo '---'
+
     echo "Build Image => $IMAGE:${TAGS_ID}"
     echo ">> docker build -t $IMAGE:${TAGS_ID} -f $FILE ."
     docker build -t $IMAGE:${TAGS_ID} -f $FILE .
+    echo '---'
+
+    echo "Build Image => $IMAGE:${BASE_IMAGE}-${TAGS_ID}"
+    echo ">> docker build -t $IMAGE:${BASE_IMAGE}-${TAGS_ID} -f $FILE ."
+    docker build -t $IMAGE:${BASE_IMAGE}-${TAGS_ID} -f $FILE .
+    echo '---'
   else
+    echo "Build Image => $IMAGE:${BASE_IMAGE}"
+    echo ">> docker build -t $IMAGE:${BASE_IMAGE} -f $FILE ."
+    docker build -t $IMAGE:${BASE_IMAGE} -f $FILE .
+    echo '---'
+
     echo "Build Image => $IMAGE:${TAGS_ID}"
     echo "docker build -t $IMAGE:${TAGS_ID} -f $FILE ."
     docker build -t $IMAGE:${TAGS_ID} -f $FILE .
+    echo '---'
+
+    echo "Build Image => $IMAGE:${BASE_IMAGE}-${TAGS_ID}"
+    echo ">> docker build -t $IMAGE:${BASE_IMAGE}-${TAGS_ID} -f $FILE ."
+    docker build -t $IMAGE:${BASE_IMAGE}-${TAGS_ID} -f $FILE .
+    echo '---'
 
     echo "Build Image => $IMAGE:${TAGS_ID}-${CUSTOM_TAGS}"
     docker build -t $IMAGE:${TAGS_ID}-${CUSTOM_TAGS} -f $FILE .
     echo ">> docker build -t $IMAGE:${TAGS_ID}-${CUSTOM_TAGS} -f $FILE ."
+    echo '---'
+
+    echo "Build Image => $IMAGE:${BASE_IMAGE}-${TAGS_ID}-${CUSTOM_TAGS}"
+    echo ">> docker build -t $IMAGE:${BASE_IMAGE}-${TAGS_ID}-${CUSTOM_TAGS} -f $FILE ."
+    docker build -t $IMAGE:${BASE_IMAGE}-${TAGS_ID}-${CUSTOM_TAGS} -f $FILE .
+    echo '---'
   fi
 }
 
 main() {
-  # docker_build alpine Dockerfile.alpine 3.16 devopscorner/nifi
-  # docker_build ubuntu Dockerfile.ubuntu 22.04 devopscorner/nifi
-  # docker_build codebuild Dockerfile.codebuild 4.0 devopscorner/nifi
-  docker_build $1 $2 $3 $4
+  # docker_build Dockerfile devopscorner/nifi [alpine|ubuntu] [version|latest|tags] [custom-tags]
+  # docker_build Dockerfile devopscorner/nifi-registry [alpine|ubuntu] [version|latest|tags] [custom-tags]
+  docker_build $1 $2 $3 $4 $5
   echo ''
   echo '-- ALL DONE --'
 }
 
 ### START HERE ###
-main $1 $2 $3 $4
+main $1 $2 $3 $4 $5
+
+### How to Execute ###
+# ./dockerhub-build.sh Dockerfile [DOCKERHUB_IMAGE_PATH] [alpine|ubuntu] [version|latest|tags] [custom-tags]
